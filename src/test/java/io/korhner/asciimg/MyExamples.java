32106ae95e333bab799c49fb90786f008df239cb
damonMPR <woolpack.cwz@gmail.com>
2017-03-15 15:42:33 +0800

    测试运行

---
+++ src/test/java/io/korhner/asciimg/MyExamples.java
@@ -0,0 +1,13 @@
+/**
+ * @(#)io.korhner.asciimg.MyExamples.java Copyright (c) 2014-2018 crimps
+ */
+package io.korhner.asciimg;
+
+/**
+ *
+ * @author crimps
+ * @version 1.0  2017/3/15
+ * @modified crimps  2017/3/15  <创建>
+ */
+public class MyExamples {
+}
